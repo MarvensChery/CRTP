83f25971c09f7d7deb94050a6c4d0800bc6ec2b6
Nathan Lamy <e2036136@cmaisonneuve.qc.ca>
2022-02-27 22:49:41 -0500

    v3

--- Serveur/app.js
+++ Serveur/app.js
@@ -1,69 +1,78 @@
-const express = require("express");
-const requeteKnex = require("./database/requeteKnex");
+const express = require('express');
+const cors = require('cors');
+const requeteKnex = require('./database/requeteKnex');
+
 const PORT = process.env.PORT || 5000;
 const app = express();
-const cors = require("cors");
 
 app.use(cors());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
-app.get("/personnes", async(req, rep) => {
-    try {
-        let personne = await requeteKnex.getPersonne();
-        rep.status(200).json(personne);
-        
-    } catch (error) {
-        rep.status(500).json();
-    }
-})
+app.get('/personnes', async (req, rep) => {
+  try {
+    const personne = await requeteKnex.getPersonne();
+    rep.status(200).json(personne);
+  } catch (error) {
+    rep.status(500).json();
+  }
+});
 
+app.get('/utilisateurs', async (req, rep) => {
+  try {
+    const utilisateurs = await requeteKnex.getUtilisateurs();
+    rep.status(200).json(utilisateurs);
+  } catch (error) {
+    rep.status(500).json();
+  }
+});
 
-app.get("/utilisateurs", async(req, rep) => {
-    try {
-        let utilisateurs = await requeteKnex.getUtilisateurs();
-        rep.status(200).json(utilisateurs);
-        
-    } catch (error) {
-        rep.status(500).json();
-    }
-})
+app.get('/ippe/:nomFamille/:prenom1/:prenom2/:masculin/:anne/:mois/:jour', async (req, rep) => {
+  try {
+    const nomFamille = req.params.nomFamille;
+    const prenom1 = req.params.prenom1;
+    const prenom2 = req.params.prenom2;
+    const masculin = req.params.masculin;
 
-app.get("/ippe/:nom/:prenom1/:prenom2/:masculin/:datenaissance", async(req, rep) => {
-    try {
-        let nom = req.params.nom
-        let prenom1 = req.params.prenom1
-        let prenom2 = req.params.prenom2
-        let masculin = req.params.masculin
-        let datenaissance = req.params.datenaissance
-                                        //'Michaud', 'Noémie', null, false, "2002-08-07T00:00:00.000Z"
-        let ippe = await requeteKnex.getIPPE(nom, prenom1, prenom2, masculin, datenaissance);
-        rep.status(200).json(ippe);
-        
-    } catch (error) {
-        rep.status(500).json();
-    }
-})
+    const anne = req.params.anne;
+    const mois = req.params.mois;
+    const jour = req.params.jour;
 
-app.get("/conditions", async(req, rep) => {
-    try {
-        let conditions = await requeteKnex.getConditions();
-        rep.status(200).json(conditions);
-        
-    } catch (error) {
-        rep.status(500).json();
-    }
-})
+    // 'Ducharme','Benoit', null, true, new Date('1975-08-31')
+    // 'Michaud', 'Noémie', null, false, new Date('2002-08-07')
+    // 'Sirois','Danielle', null, false, new Date('1980-02-14')
+    // 'Bélanger','Claude', null, true, new Date('1976-07-12')
+    //'Levasseur', 'Marc', null, true, new Date('1971-11-07')
+    //'Lemire', 'Jessy', null, false, new Date('1985-10-28')
+    //'Amoussougbo', 'Yaken', null, true, new Date('2000-03-14')
+    //'Hébert', 'Francis', null, true, new Date('1992-10-19')
+    //?nomFamille=Ducharme?prenom1=Benoit?prenom2=null?masculin=true?datenaissance=new Date('1975-08-31')
+    //nomFamille, prenom1, prenom2, masculin, datenaissance
+    ///:nomFamille/:prenom1/:prenom2/:masculin/:datenaissance
+    const ippe = await requeteKnex.getIPPE(nomFamille, prenom1, prenom2, masculin, new Date(`${anne}-${mois}-${jour}`));
+    rep.status(200).json(ippe);
+  } catch (error) {
+    rep.status(500).json();
+  }
+});
 
-app.get("/fps", async(req, rep) => {
-    try {
-        let fps = await requeteKnex.getFPS();
-        rep.status(200).json(fps);
-        
-    } catch (error) {
-        rep.status(500).json();
-    }
-})
+app.get('/conditions', async (req, rep) => {
+  try {
+    const conditions = await requeteKnex.getConditions();
+    rep.status(200).json(conditions);
+  } catch (error) {
+    rep.status(500).json();
+  }
+});
+
+app.get('/fps', async (req, rep) => {
+  try {
+    const fps = await requeteKnex.getFPS();
+    rep.status(200).json(fps);
+  } catch (error) {
+    rep.status(500).json();
+  }
+});
 
 app.listen(PORT, () => {
-    console.log("Mon application du côté serveur est en train d'exécuter sur le port " + PORT)
-})
+  console.log(`Mon application du côté serveur est en train d'exécuter sur le port ${PORT}`);
+});
--- Serveur/database/requeteKnex.js
+++ Serveur/database/requeteKnex.js
@@ -1,178 +1,178 @@
-const path = require("path");
-
-const knex = require("knex")({
-    client: "mssql",
-    connection: {
-        host: "sv55.cmaisonneuve.qc.ca",
-        user : '4D1EQUIPE06',
-        password : 'pwn852',
-        database : '4D1Equipe06'
-      },
-    useNullAsDefault: false
+const knex = require('knex')({
+  client: 'mssql',
+  connection: {
+    host: 'sv55.cmaisonneuve.qc.ca',
+    user: '4D1EQUIPE06',
+    password: 'pwn852',
+    database: '4D1Equipe06',
+  },
+  useNullAsDefault: false,
 });
 
 function getPersonne() {
-    return knex("Personnes");
+  return knex('Personnes');
 }
 
 function getUtilisateurs() {
-    return knex("Utilisateurs");
+  return knex('Utilisateurs');
 }
 
 function getConditions() {
-    return knex("Conditions");
+  return knex('Conditions');
 }
 
 function getFPS() {
-    return knex("FPS");
+  return knex('FPS');
 }
 
 function getIBAF() {
-    return knex("IBAF");
+  return knex('IBAF');
 }
 
 function getIBOB() {
-    return knex("IBOB");
+  return knex('IBOB');
 }
 
 function getIBVA() {
-    return knex("IBVA");
+  return knex('IBVA');
 }
-//nomFamille, prenom1, prenom2, masculin, dateNaissance
+// nomFamille, prenom1, prenom2, masculin, dateNaissance
 async function getIPPE(nomFamille, prenom1, prenom2, masculin, dateNaissance) {
-    
-    let resultat = await knex("Personnes")
-    //.select("NomFamille", "Prenom1", "Prenom2", "Masculin", "DateNaissance", "Raison", "Cour",  "noCour", "NatureCrime", "noEvenement")
-    .join("ippe", "Personnes.Id", "=", "IPPE.IdPersonne")
+  const resultat = await knex('Personnes')
+    // .select("NomFamille", "Prenom1", "Prenom2", "Masculin", "DateNaissance", "Raison", "Cour",  "noCour", "NatureCrime", "noEvenement")
+    .join('ippe', 'Personnes.Id', '=', 'IPPE.IdPersonne')
     .where({
-        'nomFamille' : nomFamille,
-        'prenom1' : prenom1,
-        'prenom2' : prenom2 == 'null' ? null: prenom2,
-        'masculin' : masculin,
-        'dateNaissance' : dateNaissance,
-    })
+      nomFamille,
+      prenom1,
+      prenom2: prenom2 == 'null' ? null : prenom2,
+      masculin,
+      dateNaissance,
+    });
 
-    console.log(resultat[0])
-    
-    var date = new Date(resultat[0].DateNaissance)
+  console.log(resultat[0]);
+  
+  const date = new Date(resultat[0].DateNaissance);
 
-    switch (resultat[0].TypeEvenement) {
-        case 'Recherché':
-            return {
-                nom : resultat[0].NomFamille,
-                prenom1 : resultat[0].Prenom1,
-                prenom2 : resultat[0].Prenom2,
-                sexe : resultat[0].Masculin? 'Masculin': 'Féminin',
-                dateNaissance : date.toLocaleDateString(),
-                mandat: resultat[0].Raison,
-                cour: resultat[0].Cour,
-                noMandat: resultat[0].NoCour,
-                natureCrime: resultat[0].NatureCrime,
-                noEvenement: resultat[0].NoEvenement
-            }
-        
-        case 'Sous observation':
-            return {
-                nom : resultat[0].NomFamille,
-                prenom1 : resultat[0].Prenom1,
-                prenom2 : resultat[0].Prenom2,
-                sexe : resultat[0].Masculin,
-                dateNaissance : date.toLocaleDateString(),
-                mandat: resultat[0].Raison,
-                natureCrime: resultat[0].NatureCrime,
-                noEvenement: resultat[0].NoEvenement,
-                DossierEnquete: resultat[0].DossierEnquete
-            }
+  switch (resultat[0].TypeEvenement) {
+    case 'Recherché':
+      return {
+        nomFamille: resultat[0].NomFamille,
+        prenom1: resultat[0].Prenom1,
+        prenom2: resultat[0].Prenom2,
+        sexe: resultat[0].Masculin ? 'Masculin' : 'Féminin',
+        dateNaissance: date.toLocaleDateString('zh-Hans-CN'),
+        mandat: resultat[0].Raison,
+        cour: resultat[0].Cour,
+        noMandat: resultat[0].NoCour,
+        natureCrime: resultat[0].NatureCrime,
+        noEvenement: resultat[0].NoEvenement,
+      };
 
-        case 'Accusé':
-            return {
-                nom : resultat[0].NomFamille,
-                prenom1 : resultat[0].Prenom1,
-                prenom2 : resultat[0].Prenom2,
-                sexe : resultat[0].Masculin,
-                dateNaissance : date.toLocaleDateString(),
-                cour: resultat[0].Cour,
-                noMandat: resultat[0].NoCour,
-                natureCrime: resultat[0].NatureCrime,
-                noEvenement: resultat[0].NoEvenement,
-                Conditions: resultat[0].Conditions
-            }
+    case 'Sous observation':
+      return {
+        nomFamille: resultat[0].NomFamille,
+        prenom1: resultat[0].Prenom1,
+        prenom2: resultat[0].Prenom2,
+        sexe: resultat[0].Masculin ? 'Masculin' : 'Féminin',
+        dateNaissance: date.toLocaleDateString('zh-Hans-CN'),
+        mandat: resultat[0].Raison,
+        natureCrime: resultat[0].NatureCrime,
+        noEvenement: resultat[0].NoEvenement,
+        DossierEnquete: resultat[0].DossierEnquete,
+      };
 
-        case 'Probation':
-            return {
-                nom : resultat[0].NomFamille,
-                prenom1 : resultat[0].Prenom1,
-                prenom2 : resultat[0].Prenom2,
-                sexe : resultat[0].Masculin,
-                dateNaissance : date.toLocaleDateString(),
-                cour: resultat[0].Cour,
-                noMandat: resultat[0].NoCour,
-                natureCrime: resultat[0].NatureCrime,
-                noEvenement: resultat[0].NoEvenement,
-                Conditions: resultat[0].Conditions,
-                FinSentence: resultat[0].FinSentence,
-                Agent: resultat[0].Agent,
-                Telephone: resultat[0].Telephone,
-                Poste: resultat[0].Poste
-            }
-            
-        case 'Libération conditionnelle':
-            return {
-                nom : resultat[0].NomFamille,
-                prenom1 : resultat[0].Prenom1,
-                prenom2 : resultat[0].Prenom2,
-                sexe : resultat[0].Masculin,
-                dateNaissance : date.toLocaleDateString(),
-                cour: resultat[0].Cour,
-                noMandat: resultat[0].NoCour,
-                natureCrime: resultat[0].NatureCrime,
-                noEvenement: resultat[0].NoEvenement,
-                Conditions: resultat[0].Conditions,
-                LieuDetention: resultat[0].LieuDetention,
-                FinSentence: resultat[0].FinSentence,
-                Agent: resultat[0].Agent,
-                Telephone: resultat[0].Telephone,
-                Poste: resultat[0].Poste
-            }
+    case 'Accusé':
+      return {
+        nomFamille: resultat[0].NomFamille,
+        prenom1: resultat[0].Prenom1,
+        prenom2: resultat[0].Prenom2,
+        sexe: resultat[0].Masculin ? 'Masculin' : 'Féminin',
+        dateNaissance: date.toLocaleDateString('zh-Hans-CN'),
+        cour: resultat[0].Cour,
+        noMandat: resultat[0].NoCour,
+        natureCrime: resultat[0].NatureCrime,
+        noEvenement: resultat[0].NoEvenement,
+        Conditions: resultat[0].Conditions,
+      };
 
-        case 'Disparu':
-            return {
-                nom : resultat[0].NomFamille,
-                prenom1 : resultat[0].Prenom1,
-                prenom2 : resultat[0].Prenom2,
-                sexe : resultat[0].Masculin,
-                dateNaissance : date.toLocaleDateString(),
-                mandat: resultat[0].Raison,
-                noEvenement: resultat[0].NoEvenement,
-                VuDerniereFois: resultat[0].VuDerniereFois
-            }
+    case 'Probation':
+        const datefinsentence = new Date(resultat[0].FinSentence);
+      return {
+        nomFamille: resultat[0].NomFamille,
+        prenom1: resultat[0].Prenom1,
+        prenom2: resultat[0].Prenom2,
+        sexe: resultat[0].Masculin ? 'Masculin' : 'Féminin',
+        dateNaissance: date.toLocaleDateString('zh-Hans-CN'),
+        cour: resultat[0].Cour,
+        noMandat: resultat[0].NoCour,
+        natureCrime: resultat[0].NatureCrime,
+        noEvenement: resultat[0].NoEvenement,
+        Conditions: resultat[0].Conditions,
+        FinSentence: datefinsentence.toLocaleDateString('zh-Hans-CN'),
+        Agent: resultat[0].Agent,
+        Telephone: resultat[0].Telephone,
+        Poste: resultat[0].Poste,
+      };
 
-        case 'Interdit':
-            return {
-                nom : resultat[0].NomFamille,
-                prenom1 : resultat[0].Prenom1,
-                prenom2 : resultat[0].Prenom2,
-                sexe : resultat[0].Masculin,
-                dateNaissance : date.toLocaleDateString('en-US'),
-                mandat: resultat[0].Raison,
-                cour: resultat[0].Cour,
-                noMandat: resultat[0].NoCour,
-                natureCrime: resultat[0].NatureCrime,
-                noEvenement: resultat[0].NoEvenement,
-                FinSentence: resultat[0].FinSentence
-            }
-    }
+    case 'Libération Conditionnelle':
+        const datefinsentencee = new Date(resultat[0].FinSentence);
+      return {
+        nomFamille: resultat[0].NomFamille,
+        prenom1: resultat[0].Prenom1,
+        prenom2: resultat[0].Prenom2,
+        sexe: resultat[0].Masculin ? 'Masculin' : 'Féminin',
+        dateNaissance: date.toLocaleDateString('zh-Hans-CN'),
+        cour: resultat[0].Cour,
+        noMandat: resultat[0].NoCour,
+        natureCrime: resultat[0].NatureCrime,
+        noEvenement: resultat[0].NoEvenement,
+        //Conditions: resultat[0].Conditions,
+        LieuDetention: resultat[0].LieuDetention,
+        FinSentence: datefinsentencee.toLocaleDateString('zh-Hans-CN'),
+        Agent: resultat[0].Agent,
+        Telephone: resultat[0].Telephone,
+        Poste: resultat[0].Poste,
+      };
 
-    return finalresultat
+    case 'Disparu':
+      return {
+        nomFamille: resultat[0].NomFamille,
+        prenom1: resultat[0].Prenom1,
+        prenom2: resultat[0].Prenom2,
+        sexe: resultat[0].Masculin ? 'Masculin' : 'Féminin',
+        dateNaissance: date.toLocaleDateString('zh-Hans-CN'),
+        mandat: resultat[0].Raison,
+        noEvenement: resultat[0].NoEvenement,
+        VuDerniereFois: resultat[0].VuDerniereFois,
+      };
+
+    case 'Interdit':
+        const datefinsentenceee = new Date(resultat[0].FinSentence);
+      return {
+        nomFamille: resultat[0].NomFamille,
+        prenom1: resultat[0].Prenom1,
+        prenom2: resultat[0].Prenom2,
+        sexe: resultat[0].Masculin ? 'Masculin' : 'Féminin',
+        dateNaissance: date.toLocaleDateString('zh-Hans-CN'),
+        mandat: resultat[0].Raison,
+        cour: resultat[0].Cour,
+        noMandat: resultat[0].NoCour,
+        natureCrime: resultat[0].NatureCrime,
+        noEvenement: resultat[0].NoEvenement,
+        FinSentence: datefinsentenceee.toLocaleDateString('zh-Hans-CN'),
+      };
+  }
+
+  return finalresultat;
 }
 
 module.exports = {
-    getPersonne,
-    getUtilisateurs,
-    getConditions,
-    getFPS,
-    getIBAF,
-    getIBOB,
-    getIBVA,
-    getIPPE,
-}
+  getPersonne,
+  getUtilisateurs,
+  getConditions,
+  getFPS,
+  getIBAF,
+  getIBOB,
+  getIBVA,
+  getIPPE,
+};
