88c3fe83b904c1d3d93e2fdea0455323b9b8e8c3
samy <e2072931@cmaisonneuve.qc.ca>
2022-04-24 05:16:57 -0400

    valeur update

--- routes/valeurs.js
+++ routes/valeurs.js
@@ -10,15 +10,15 @@ router.get('/:idValeur', async (req, res) => {
     try {
         let data;
         if (req.params.idValeur !== undefined) data = await request.getDataById(req.params.idValeur);
-        else return res.status(400).json('paramètre manquant');
+        else return res.status(400).json({ message: 'paramètre manquant', success: false });
         if (data.length === 0) {
             // retourne la valeur negative
-            return res.status(404).json({ message: 'aucune donnée trouvé' });
+            return res.status(404).json({ message: 'aucune donnée trouvé', success: false });
         }
         // retourne que les valeurs au client;
         return res.status(200).json(data);
     } catch (error) {
-        return res.status(500).json(error.message);
+        return res.status(500).json({ message: error.message, success: false });
     }
 });
 
@@ -27,12 +27,12 @@ router.get('/', async (req, res) => {
         const data = await request.getValeursAll();
         if (data.length === 0) {
             // retourne la valeur negative
-            return res.status(404).json({ message: 'aucune donnée trouvé' });
+            return res.status(404).json({ message: 'aucune donnée trouvé', success: false });
         }
         // retourne que les valeurs au client;
         return res.status(200).json(data);
     } catch (error) {
-        return res.status(500).json(error.message);
+        return res.status(500).json({ message: error.message, success: false });
     }
 });
 
@@ -40,12 +40,12 @@ router.post('/', async (req, res) => {
     // choix des infos a envoyer selon la banque de données choisi
     try {
         if (req.body.NoSerie === undefined || req.body.auteur === undefined || req.body.typeVa === undefined
-            || req.body.resIBVA === undefined || req.body.NoEvenement === undefined) return res.status(400).json('paramètre manquant');
+            || req.body.resIBVA === undefined || req.body.NoEvenement === undefined) return res.status(400).json({ message: 'paramètre manquant', success: false });
 
         // verifie si l'entite a ajouter existe deja dans la base de donnees
         const DataAdd = await request.getDataByNoEvenement(req.body.NoEvenement);
         // si oui renvoyer une errer
-        if (DataAdd.length !== 0) return res.status(404).json({ message: 'l\'entité se trouve déja dans la base de donnée' });
+        if (DataAdd.length !== 0) return res.status(404).json({ message: 'l\'entité se trouve déja dans la base de donnée', success: false });
 
         const DataToSend = {
             Identifiant: req.body.NoSerie,
@@ -58,23 +58,24 @@ router.post('/', async (req, res) => {
         await request.postValeur(DataToSend);
         // avoir le id de la nouvelle entité
         const Data = await request.getDataByNoEvenement(req.body.NoEvenement);
-        if (Data.length === 0) return res.status(404).json({ message: 'aucune donnée trouvé' });
-        return res.status(200).json({ message: `L’entité a été ajoutée avec succès Id: ${Data[0].IdIBVA}` });
+        if (Data.length === 0) return res.status(404).json({ message: 'aucune donnée trouvé', success: false });
+        return res.status(200).json({ message: `L’entité a été ajoutée avec succès Id: ${Data[0].IdIBVA}`, success: true });
     } catch (error) {
-        return res.status(500).json(error.message);
+        return res.status(500).json({ message: error.message, success: false });
     }
 });
 
 // route pour modifier les donnees dans la base
 router.put('/:idValeur', async (req, res) => {
     try {
+        console.log(req.params.idValeur);
         if (req.body.NoSerie === undefined || req.body.auteur === undefined || req.body.typeVa === undefined
-            || req.body.resIBVA === undefined || req.body.NoEvenement === undefined) return res.status(400).json('paramètre manquant');
+            || req.body.resIBVA === undefined || req.body.NoEvenement === undefined) return res.status(400).json({ message: 'paramètre manquant', success: false });
 
         // verifier si l'entite est deja dans la base de donnees
         const DataAdd = await request.getDataById(req.params.idValeur);
         // si non renvoye une errer
-        if (DataAdd.length === 0) return res.status(404).json({ message: 'l\'entité n\'existe pas dans la base de donnée' });
+        if (DataAdd.length === 0) return res.status(404).json({ message: 'l\'entité n\'existe pas dans la base de donnée', success: false });
 
         const DataToSend = {
             Identifiant: req.body.NoSerie,
@@ -85,9 +86,9 @@ router.put('/:idValeur', async (req, res) => {
         };
         // donner en parametre le type de la table/ les donnees a update/ et le id de l'entite a update
         await request.updateValeur(DataToSend, req.params.idValeur);
-        return res.status(200).json({ message: 'L’entité a été modifié avec succès' });
+        return res.status(200).json({ message: 'L’entité a été modifié avec succès', success: true });
     } catch (error) {
-        return res.status(500).json(error.message);
+        return res.status(500).json({ message: error.message, success: false });
     }
 });
 
@@ -98,14 +99,14 @@ router.delete('/:idValeur', async (req, res) => {
         data = await request.getDataById(req.params.idValeur);
         if (data.length === 0) {
             // retourne message d'errer
-            return res.status(404).json({ message: 'aucune donnée trouvé' });
+            return res.status(404).json({ message: 'aucune donnée trouvé', success: false });
         }
 
         await request.deleteData(req.params.idValeur);
         // retourne une confirmation
-        return res.status(200).json({ message: 'l\'objet a bien été supprimé' });
+        return res.status(200).json({ message: 'l\'objet a bien été supprimé', success: true });
     } catch (error) {
-        return res.status(500).json(error.message);
+        return res.status(500).json({ message: error.message, success: false });
     }
 });
 
