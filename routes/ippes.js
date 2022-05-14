21a3e47a9a09c46774bc842ae96583efb926c07d
Anis <e1940695@cmaisonneuve.qc.ca>
2022-05-13 23:33:38 -0400

    Update ippes.js

--- routes/ippes.js
+++ routes/ippes.js
@@ -42,48 +42,6 @@ router.get('/info', async (req, res) => {
     return res.status(200).json(resultat);
 });
 
-router.get('/event', async (req, res) => {
-    try {
-        // requetes:
-        // trouver une personne a partir du num event
-        console.log('test');
-        console.log(req.query.noEvent);
-
-        const pers = (await request.chercherPersonne(req.query.noEvent))[0];
-        console.log('done');
-
-        console.log(pers);
-
-        // trouver un event a partir de son noEvent
-        const event = (await request.chercherEvent(req.query.noEvent))[0];
-        console.log(event);
-        if (!event || !pers) {
-            return res.status(404);
-        }
-        // trouver les conditions d'un evenement a partir de son noEvent
-        const conditions = await request.chercherConditions(req.query.noEvent);
-        console.log(conditions);
-        const data = {
-            type: event.TypeEvenement,
-            personne: {
-                nomFamille: pers.NomFamille,
-                prenom1: pers.Prenom1,
-                prenom2: pers.Prenom2,
-                sex: pers.Masculin,
-                date: pers.DateNaissance,
-            },
-            mandat: event.Mandat,
-            Cour: event.Cour,
-            noMandat: event.NoMandat,
-            natCrime: event.NatureCrime,
-            conditions,
-        };
-        console.log(data);
-        return res.status(200).json(data);
-    } catch (error) {
-        return res.status(404).json(error);
-    }
-});
 //
 // route to update an IPPE
 router.put('/:idIppe', async (req, res) => {
