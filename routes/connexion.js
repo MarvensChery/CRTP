45ae082c1b4f16dbb58c61d142d9e174475c2fde
Maximilien Désilets <e2129358@cmaisonneuve.qc.ca>
2023-03-30 15:56:13 -0400

    Cleanup

--- routes/connexion.js
+++ routes/connexion.js
@@ -1,75 +1,38 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-
 const bcrypt = require('bcryptjs');
-
-const request = require('../database/utilisateurs');
+const db = require('../database/utilisateurs');
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    let resultat;
+
+
+router.post('/', async (req, res) => {
     try {
-        resultat = await request.getUtilisateursAll();
-    } catch (error) {
-        return res.status(500).json(error.message);
-    }
-    return res.status(200).json(resultat);
-});
-
-router.post('/token', async (req, res) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    const { identifiant, motDePasse } = req.body;
-    const resultat = await request.getUtilisateurByIdentifiant(identifiant);
-    console.log(resultat);
-    if (!resultat || resultat.length === 0) {
-        return res.status(404).json({ succes: false, message: 'Identifiant incorrect' });
-    }
-    const match = await bcrypt.compare(motDePasse, resultat.MotDePasse);
-    if (!match) {
-        return res.status(404).json({ succes: false, message: 'Mot de passe incorrect' });
-    }
-    const expiresIn = 14400;
-    const accessToken = jwt.sign({ identifiant: resultat.Identifiant }, process.env.TOKEN_KEY, {
-        expiresIn,
-    });
-
-    return res.status(200).json({
-        succes: true,
-        Etudiant: resultat.Etudiant,
-        Matricule: resultat.Identifiant,
-        Nom: resultat.NomFamille,
-        access_token: accessToken,
-        expires_in: expiresIn,
-    });
-});
-
-router.post('/inscription', async (req, res) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    try {
-        const {
-            Identifiant, MotDePasse, studentOrProf, NomFamille,
-        } = req.body;
-        const MdpHash = await bcrypt.hash(MotDePasse, 10);
-        const utilisateur = await request.inscription(Identifiant, MdpHash, studentOrProf, NomFamille);
+        res.header('Access-Control-Allow-Origin', '*');
+        const { Identifiant, MotDePasse } = req.body;
+        const utilisateur = await db.getUtilisateurByIdentifiant(Identifiant);
+        if (!utilisateur) {
+            return res.status(404).json({ message: 'Identifiant incorrect' });
+        }
+        if (!bcrypt.compareSync(MotDePasse, utilisateur.MotDePasse)) {
+            return res.status(404).json({ message: 'Mot de passe incorrect' });
+        }
         const expiresIn = 14400;
+        console.log(expiresIn, process.env.TOKEN_KEY, utilisateur.Identifiant)
+
         const accessToken = jwt.sign(
             { identifiant: utilisateur.Identifiant },
             process.env.TOKEN_KEY,
             { expiresIn },
         );
         return res.status(200).json({
-            succes: true,
             Etudiant: utilisateur.Etudiant,
             Matricule: utilisateur.Identifiant,
-            Nom: utilisateur.NomFamille,
             access_token: accessToken,
             expires_in: expiresIn,
         });
-    } catch (error) {
-        return res.status(500).json(error);
-    }
+    } catch (error) { return res.status(500).json({ message: error.message }); }
 });
 
 module.exports = router;
--- routes/utilisateurs.js
+++ routes/utilisateurs.js
@@ -1,24 +1,15 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
-
-const request = require('../database/utilisateurs');
+const db = require('../database/utilisateurs');
 
 const router = express.Router();
 
 router.post('/', async (req, res) => {
     res.header('Access-Control-Allow-Origin', '*');
     try {
-        const {
-            identifiant, motDePasse, studentOrProf, nomFamille,
-        } = req.body;
-        const mdpHash = await bcrypt.hash(motDePasse, 10);
-        const utilisateur = await request.inscription(
-            identifiant,
-            mdpHash,
-            studentOrProf,
-            nomFamille,
-        );
+        req.body.MotDePasse = await bcrypt.hash(req.body.MotDePasse, 10);
+        const utilisateur = await db.inscription(req.body);
         const expiresIn = 14400;
         const accessToken = jwt.sign(
             { identifiant: utilisateur.Identifiant },
@@ -26,16 +17,12 @@ router.post('/', async (req, res) => {
             { expiresIn },
         );
         return res.status(200).json({
-            succes: true,
             Etudiant: utilisateur.Etudiant,
             Matricule: utilisateur.Identifiant,
-            Nom: utilisateur.NomFamille,
             access_token: accessToken,
             expires_in: expiresIn,
         });
-    } catch (error) {
-        return res.status(500).json(error);
-    }
+    } catch (error) { return res.status(500).json({ message: error.message }); }
 });
 
 module.exports = router;
