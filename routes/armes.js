ca88576cd91399ed722889d739b88e255f37055a
e2187263 <e2187263@cmaisonneuve.qc.ca>
2023-04-03 01:13:46 -0400

    Protection sur les PUT, GET et DELETE

--- routes/armes.js
+++ routes/armes.js
@@ -7,18 +7,22 @@ const request = require('../database/armes');
 const router = express.Router();
 // Requete pour obtenir idArme.
 router.get('/:idArme', async (req, res) => {
-    try {
-        let data;
-        if (req.params.idArme !== undefined) data = await request.getArmeById(req.params.idArme);
-        else return res.status(400).json({ message: 'Paramètre manquant', success: false });
-        if (data.length === 0) {
+    if (+(req.params.idArme)) {
+        try {
+            let data;
+            if (req.params.idArme !== undefined) data = await request.getArmeById(req.params.idArme);
+            else return res.status(400).json({ message: 'Paramètre manquant', success: false });
+            if (data.length === 0) {
             // retourne la valeur negative.
-            return res.status(404).json({ message: 'Aucune donnée trouvée', success: false });
+                return res.status(404).json({ message: 'Aucune donnée trouvée', success: false });
+            }
+            // retourne que les valeurs au client;
+            return res.status(200).json(data);
+        } catch (error) {
+            return res.status(500).json({ message: error.message, success: false });
         }
-        // retourne que les valeurs au client;
-        return res.status(200).json(data);
-    } catch (error) {
-        return res.status(500).json({ message: error.message, success: false });
+    } else {
+        return res.status(400).json({ message: 'Mauvaise formulation', success: false });
     }
 });
 // Requete pour obtenir les valeurs et retourne valeur.
@@ -66,46 +70,54 @@ router.post('/', async (req, res) => {
 
 // route pour modifier les donnees dans la base.
 router.put('/:idArme', async (req, res) => {
-    try {
-        if (req.body.NoSerie === undefined || req.body.marque === undefined || req.body.calibre === undefined
+    if (+(req.params.idArme)) {
+        try {
+            if (req.body.NoSerie === undefined || req.body.marque === undefined || req.body.calibre === undefined
             || req.body.typeAr === undefined || req.body.NoEvenement === undefined) return res.status(400).json({ message: 'Paramètre manquant', success: false });
 
-        // verifier si l'entite est deja dans la base de donnees.
-        const DataAdd = await request.getArmeById(req.params.idArme);
-        // si non renvoye une erreur
-        if (DataAdd.length === 0) return res.status(404).json({ message: 'L\'entité n\'existe pas dans la base de données', success: false });
+            // verifier si l'entite est deja dans la base de donnees.
+            const DataAdd = await request.getArmeById(req.params.idArme);
+            // si non renvoye une erreur
+            if (DataAdd.length === 0) return res.status(404).json({ message: 'L\'entité n\'existe pas dans la base de données', success: false });
 
-        const DataToSend = {
-            NoSerie: req.body.NoSerie,
-            Marque: req.body.marque,
-            Calibre: req.body.calibre,
-            TypeArme: req.body.typeAr,
-            NoEvenement: req.body.NoEvenement,
-        };
-        // donner en parametre le type de la table/ les donnees a update/ et le id de l'entite a update.
-        const reponse = await request.updateArme(DataToSend, req.params.idArme);
-        console.log(reponse);
-        return res.status(200).json({ message: 'L’entité a été modifié avec succès', success: true });
-    } catch (error) {
-        return res.status(500).json({ message: error.message, success: false });
+            const DataToSend = {
+                NoSerie: req.body.NoSerie,
+                Marque: req.body.marque,
+                Calibre: req.body.calibre,
+                TypeArme: req.body.typeAr,
+                NoEvenement: req.body.NoEvenement,
+            };
+            // donner en parametre le type de la table/ les donnees a update/ et le id de l'entite a update.
+            const reponse = await request.updateArme(DataToSend, req.params.idArme);
+            console.log(reponse);
+            return res.status(200).json({ message: 'L’entité a été modifié avec succès', success: true });
+        } catch (error) {
+            return res.status(500).json({ message: error.message, success: false });
+        }
+    } else {
+        return res.status(400).json({ message: 'Mauvaise formulation', success: false });
     }
 });
 
 // route pour delete l'entité.
 router.delete('/:idArme', async (req, res) => {
-    let data;
-    try {
-        data = await request.getArmeById(req.params.idArme);
-        if (data.length === 0) {
+    if (+(req.params.idArme)) {
+        let data;
+        try {
+            data = await request.getArmeById(req.params.idArme);
+            if (data.length === 0) {
             // retourne message d'erreur.
-            return res.status(404).json({ message: 'Aucune donnée trouvée', success: false });
-        }
+                return res.status(404).json({ message: 'Aucune donnée trouvée', success: false });
+            }
 
-        await request.deleteArme(req.params.idArme);
-        // retourne une confirmation.
-        return res.status(200).json({ message: 'L\'objet a bien été supprimé', success: true });
-    } catch (error) {
-        return res.status(500).json({ message: error.message, success: false });
+            await request.deleteArme(req.params.idArme);
+            // retourne une confirmation.
+            return res.status(200).json({ message: 'L\'objet a bien été supprimé', success: true });
+        } catch (error) {
+            return res.status(500).json({ message: error.message, success: false });
+        }
+    } else {
+        return res.status(400).json({ message: 'Mauvaise formulation', success: false });
     }
 });
 // exporte le module.
--- routes/utilisateurs.js
+++ routes/utilisateurs.js
@@ -23,57 +23,68 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
     res.header('Access-Control-Allow-Origin', '*');
+    if (+(req.params.id)) {
+        let resultat;
+        try {
+            resultat = await request.getUtilisateurById(req.params.id);
+        } catch (error) {
+            res.status(500).json(error.message);
+        }
 
-    let resultat;
-    try {
-        resultat = await request.getUtilisateurById(req.params.id);
-    } catch (error) {
-        res.status(500).json(error.message);
+        return res.status(200).json(resultat);
     }
 
-    return res.status(200).json(resultat);
+    return res.status(400).json({ message: 'Mauvaise formulation', success: false });
 });
 // route pour delete l'entité.
 router.delete('/delete/:IdUtilisateur', async (req, res) => {
-    let data;
-    try {
-        data = await request.getUtilisateurById(req.params.IdUtilisateur);
-        if (data.length === 0) {
+    if (+(req.params.IdUtilisateur)) {
+        let data;
+        try {
+            data = await request.getUtilisateurById(req.params.IdUtilisateur);
+            if (data.length === 0) {
             // retourne message d'erreur.
-            return res.status(404).json({ message: 'Aucune donnée trouvée', success: false });
-        }
+                return res.status(404).json({ message: 'Aucune donnée trouvée', success: false });
+            }
 
-        await request.deleteUtilisateurs(req.params.IdUtilisateur);
-        // retourne une confirmation.
-        return res.status(200).json({ message: 'L\'objet a bien été supprimé', success: true });
-    } catch (error) {
-        return res.status(500).json({ message: error.message, success: false });
+            await request.deleteUtilisateurs(req.params.IdUtilisateur);
+            // retourne une confirmation.
+            return res.status(200).json({ message: 'L\'objet a bien été supprimé', success: true });
+        } catch (error) {
+            return res.status(500).json({ message: error.message, success: false });
+        }
+    } else {
+        return res.status(400).json({ message: 'Mauvaise formulation', success: false });
     }
 });
 
 // update user
 router.put('/update/:IdUtilisateur', async (req, res) => {
-    try {
-        if (req.body.Identifiant === undefined || req.body.MotDePasse === undefined
+    if (+(req.params.IdUtilisateur)) {
+        try {
+            if (req.body.Identifiant === undefined || req.body.MotDePasse === undefined
             || req.body.Etudiant === undefined) return res.status(400).json({ message: 'Paramètre manquant', success: false });
 
-        // verifier si l'entite est deja dans la base de donnees.
-        const DataAdd = await request.getUtilisateurById(req.params.IdUtilisateur);
-        // si non renvoye une erreur
-        if (DataAdd.length === 0) return res.status(404).json({ message: 'l\'entité n\'existe pas dans la base de donnée', success: false });
-        const hashedPassword = await bcrypt.hash(req.body.MotDePasse, 10);
-        const DataToSend = {
+            // verifier si l'entite est deja dans la base de donnees.
+            const DataAdd = await request.getUtilisateurById(req.params.IdUtilisateur);
+            // si non renvoye une erreur
+            if (DataAdd.length === 0) return res.status(404).json({ message: 'l\'entité n\'existe pas dans la base de donnée', success: false });
+            const hashedPassword = await bcrypt.hash(req.body.MotDePasse, 10);
+            const DataToSend = {
 
-            Identifiant: req.body.Identifiant,
-            MotDePasse: hashedPassword,
-            Etudiant: req.body.Etudiant,
-        };
-        /* donner en parametre le type de la table/ les donnees a update
+                Identifiant: req.body.Identifiant,
+                MotDePasse: hashedPassword,
+                Etudiant: req.body.Etudiant,
+            };
+            /* donner en parametre le type de la table/ les donnees a update
         / et le id de l'entite a update. */
-        await request.updateUtilisateurs(DataToSend, req.params.IdUtilisateur);
-        return res.status(200).json({ message: 'L’entité a été modifié avec succès', success: true });
-    } catch (error) {
-        return res.status(500).json({ message: error.message, success: false });
+            await request.updateUtilisateurs(DataToSend, req.params.IdUtilisateur);
+            return res.status(200).json({ message: 'L’entité a été modifié avec succès', success: true });
+        } catch (error) {
+            return res.status(500).json({ message: error.message, success: false });
+        }
+    } else {
+        return res.status(400).json({ message: 'Mauvaise formulation', success: false });
     }
 });
 
