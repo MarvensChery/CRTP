e0f8fb498a8234f6d612e65701e2f8b0dc1f6f84
Marc <mlevasseur@cmaisonneuve.qc.ca>
2023-04-17 09:00:31 -0400

    Séparation des tests

--- database/ippes.test.js
+++ database/ippes.test.js
@@ -1,5 +1,4 @@
 const { getIPPE } = require('./ippes');
-const { getArmesAll, getArmeById } = require('./armes');
 const { getIppePersonne } = require('./personnes');
 
 // test Vérifie la fonction getIPPE
@@ -29,34 +28,5 @@ describe('getIppePersonne()', () => {
     });
     it('devrait retourner une liste d\'objet', async () => {
         expect(typeof personne[0]).toBe('object');
-    });
-});
-// verifier la fonction getArmeAll
-describe('getArmesAll()', () => {
-    let armes;
-    beforeEach(async () => { armes = await getArmesAll(); });
-    it('devrait avoir la propriété IdIBAF sur le premier objet de la liste', async () => {
-        expect(armes[0]).toHaveProperty('IdIBAF');
-    });
-    it('devrait retourner une liste qui n\'est pas vide', async () => {
-        expect(armes.length).toBeGreaterThan(0);
-    });
-    it('devrait retourner un tableau', async () => {
-        expect(Array.isArray(armes)).toBe(true);
-    });
-    it('devrait retourner une liste d\'objet', async () => {
-        expect(typeof armes[0]).toBe('object');
-    });
-});
-
-// verifier la fonction getArmeid
-describe('getArmeById()', () => {
-    let armesId;
-    beforeEach(async () => { armesId = await getArmeById(1); });
-    it('devrait avoir la propriété IdIBAF sur le premier objet de la liste', async () => {
-        expect(armesId).toHaveProperty('IdIBAF');
-    });
-    it('devrait retourner un objet', async () => {
-        expect(typeof armesId).toBe('object');
     });
 });
